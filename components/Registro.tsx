255fda232788d2247bda97c69e2f42d4e6f63530
JuanUT <jdcruzt@ut.edu.co>
2024-10-10 21:40:02 -0500

    feat: :sparkles: Añadido componente de registro con formulario y conexión al backend

---
+++ components/Registro.tsx
@@ -0,0 +1,78 @@
+// src/components/Registro.tsx
+import React, { useState } from 'react';
+import { IRegistro } from '../interfaces/IRegistro.interface';
+
+const Registro: React.FC = () => {
+  const [formData, setFormData] = useState<IRegistro>({
+    nombre: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3001/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log('Usuario registrado:', data);
+    } catch (error) {
+      console.error('Error al registrar usuario:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+          type="text"
+          id="nombre"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit">Registrarse</button>
+    </form>
+  );
+};
+
+export default Registro;
---
+++ interfaces/IRegistro.interface.ts
@@ -0,0 +1,7 @@
+// src/interfaces/UsuarioProps.ts
+export interface IRegistro {
+    nombre: string;
+    email: string;
+    password: string;
+  }
+  
