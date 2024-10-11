e4c9a5507854c65cbb926c9a2de92c4f6111521d
JuanUT <jdcruzt@ut.edu.co>
2024-10-10 23:41:35 -0500

    refactor: :recycle: Conectando a api railway

--- components/Registro.tsx
+++ components/Registro.tsx
@@ -20,7 +20,7 @@ const Registro: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3001/', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
