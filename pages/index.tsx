866cf4a31856879c09d9bc71bfbfe11bbc9426a6
JuanUT <jdcruzt@ut.edu.co>
2024-10-10 21:52:04 -0500

    Intentando desplegar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
 import localFont from "next/font/local";
 import Registro from "@/components/Registro";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export default function Home() {
   return (
     <>
